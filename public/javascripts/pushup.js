b792ad37749571aa15c63bf2acf5f4f9efa27c02
obabichev <aq.prots@gmail.com>
2016-12-02 18:13:33 +0300

    small fix of graph

--- public/javascripts/pushup.js
+++ public/javascripts/pushup.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
         options: {
             scales: {
                 xAxes: [{
-                    type: 'linear',
+                    type: 'time',
                     position: 'bottom'
                 }]
             }
